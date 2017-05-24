4cc342ca0add26ee435e1cd4094415a32b388afc
me <webmaster@emolio.is>
2017-05-24 23:23:22 +0200

    dded stones and added list of viruses for screen resolution 1024

---
+++ methapia-stone-fyto-meta-1024x.sikuli/methapia-stone-fyto-meta-1024x.py
@@ -0,0 +1,70 @@
+
+#click("1494363659321.png")
+
+#for i in range(9):
+#    if exists("1494368870260.png"):
+#        click("1494368870260.png")
+#        sleep(3)
+#on dna and rna level there is no stone therapy
+#sleep(20)
+click("1494363684711.png")
+#click(Pattern("1494363737993.png").targetOffset(-68,-1))
+#for i in range(2):
+
+wait("1494495917879.png", 600)
+click("1494495917879.png")
+#wait("1494365357200.png", 300)
+#click("1494365357200.png") 
+
+click(Pattern("1494363737993.png").targetOffset(-68,-1)) 
+count = 0
+for i in range(3):
+    print count
+    click("1494364888480.png")
+    
+    if count == 1:
+        type(Key.DOWN)
+    wait("1494363779474.png", 30*60)
+    click("1494363779474.png")
+    wait("1494496066918.png", 600)
+    click("1494496066918.png") 
+    #wait("1494363872024.png", 600)
+    #click("1494363872024.png")
+    wait("1494363924147.png", 30*60)
+    click(Pattern("1494363924147.png").targetOffset(46,28))
+    count = 1
+
+
+#wait("1494365357200.png", 30*60)
+#click("1494365357200.png") 
+
+
+click(Pattern("1494365448806.png").targetOffset(-80,-1))
+
+
+count = 0
+for i in range(3):
+    print count
+    click("1494364888480.png")
+    
+    if count == 1:
+        type(Key.DOWN)
+    wait("1494365200537.png", 30*60)
+    
+    click("1494365200537.png")
+    wait("1494363872024.png", 30*60)
+    click("1494363872024.png")
+    wait("1494363924147.png", 30*60)
+    click(Pattern("1494363924147.png").targetOffset(46,28))
+    count = 1
+
+click("1494365724204.png")
+wait("1494365744091.png", 30*60)
+click("1494365744091.png")
+wait(Pattern("1494366439552.png").targetOffset(62,-6), 30*60)
+click(Pattern("1494366439552.png").targetOffset(62,-6))
+
+#click("1494365795747.png")#TODO fix there are two of them 
+wait("1494363924147.png", 30*60)
+click(Pattern("1494363924147.png").targetOffset(46,28))
+
---
+++ methapia-stone-fyto-meta.sikuli/methapia-stone-fyto-meta.py
@@ -0,0 +1,79 @@
+
+#click("1494363659321.png")
+
+#for i in range(9):
+#    if exists("1494368870260.png"):
+#        click("1494368870260.png")
+#        sleep(3)
+#on dna and rna level there is no stone therapy
+#sleep(20)
+click("1494496665247.png")
+
+#click("1494363684711.png")
+#click(Pattern("1494363737993.png").targetOffset(-68,-1))
+#for i in range(2):
+wait("1494496708347.png", 600)
+click("1494496708347.png")
+#wait("1494495917879.png", 600)
+#click("1494495917879.png")
+#wait("1494365357200.png", 300)
+#click("1494365357200.png") 
+
+click(Pattern("1494363737993.png").targetOffset(-68,-1)) 
+count = 0
+for i in range(3):
+    print count
+    click("1494364888480.png")
+    
+    if count == 1:
+        type(Key.DOWN)
+    wait("1494363779474.png", 30*60)
+    click("1494363779474.png")
+
+    wait("1494496782216.png", 2000)
+
+    click("1494496782216.png")
+
+    wait("1494363924147.png", 30*60)
+    click(Pattern("1494363924147.png").targetOffset(46,28))
+    count = 1
+
+
+#wait("1494365357200.png", 30*60)
+#click("1494365357200.png") 
+wait("1494496708347.png", 600)
+click("1494496708347.png")
+
+click(Pattern("1494365448806.png").targetOffset(-80,-1))
+
+
+count = 0
+for i in range(3):
+    print count
+    click("1494364888480.png")
+    
+    if count == 1:
+        type(Key.DOWN)
+    wait("1494365200537.png", 30*60)
+    
+    click("1494365200537.png")
+    wait("1494496782216.png", 2000)
+
+    click("1494496782216.png")
+    wait("1494363924147.png", 30*60)
+    click(Pattern("1494363924147.png").targetOffset(46,28))
+    count = 1
+
+click("1494365724204.png")
+wait("1494365744091.png", 30*60)
+click("1494365744091.png")
+wait("1494497188692.png", 300)
+click(Pattern("1494497188692.png").targetOffset(64,-2))
+
+#wait(Pattern("1494366439552.png").targetOffset(62,-6), 30*60)
+#click(Pattern("1494366439552.png").targetOffset(62,-6))
+
+#click("1494365795747.png")#TODO fix there are two of them 
+wait("1494363924147.png", 30*60)
+click(Pattern("1494363924147.png").targetOffset(46,28))
+
